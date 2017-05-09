61be3a923da533a0fc8ba65268ac69d249b57183
Brandon <brantheif115@gmail.com>
2017-05-09 11:10:40 -0400

    Committing updates:
    Started implementing the LinkedList and Node class

--- src/LinearLinkedList.java
+++ src/LinearLinkedList.java
@@ -19,17 +19,26 @@ class LinearLinkedList {
     public void add(CompletionEvent completionEvent) {
 
         //Special case, list empty, standard insertion at first.
+        if ( head == null ) {
+            Node newNode = new Node(completionEvent);
+            newNode.setNext(null);
+            head = newNode;
+        } else {
 
-        //Find "curr" and "prev" to perform insertion. These are temp variables.
-        completionEvent curr = head;
-        completionEvent prev = null;
-        //Update prev and curr according to the sorting
-        while ()
+            //Find "curr" and "prev" to perform insertion. These are temp variables.
+            completionEvent curr = head;
+            completionEvent prev = null;
 
+            //Update prev and curr according to the sorting
+            //We only add a node to the list where there are no younger CompletionEvents than itself
+            while ( ( curr != null) && curr.isOlder( completionEvent ) ) {
+                prev = curr;
+                curr = curr.getNext();
+            }
 
-        Node newNode = new Node( completionEvent );
-        newNode.setNext( curr );
-        prev.setNext( newNode );
-
+            Node newNode = new Node( completionEvent );
+            newNode.setNext( curr );
+            prev.setNext( newNode );
+        }
     }
 }
---
+++ src/LinearLinkedNode.java
@@ -0,0 +1,21 @@
+/**
+ * Created by brandon on 5/9/17.
+ */
+public class LinearLinkedNode {
+
+    private Object item;
+    private Object next;
+
+    //Default constructor
+    public LinearLinkedNode() {
+        item = null;
+        next = null;
+    }
+
+    public boolean isOlder( Object obj ) {
+        //Compare the dates of this "this.item"
+
+        return true;
+    }
+}
+
