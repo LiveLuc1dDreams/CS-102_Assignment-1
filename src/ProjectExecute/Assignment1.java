76a20a012ae071ca81a21b9a441648cccf35fd24
Brandon <brantheif115@gmail.com>
2017-05-08 23:38:36 -0400

    Committing updates:
    Added more comments.

--- src/ProjectExecute/Assignment1.java
+++ src/ProjectExecute/Assignment1.java
@@ -10,8 +10,13 @@ import java.io.File;
 /**
  *This class will be the main method that runs the University database
  */
+
+//General todo items:
+    //TODO: add method to change the location of the input file
 public class Assignment1 {
 
+    //Here all of the commands to create the database, read the input file,
+    // add the items from the input file to the database, and take commands from the user.
     public static void main(String[] args) throws UniversityDatabaseException, FileNotFoundException {
 
         //File object to be used to read the input for the database from a text file
@@ -41,27 +46,36 @@ public class Assignment1 {
                 "9 --> Exit");
     }
 
-    //this method will be used to add all the items found in the
+    //This method will be used to add all the items found in the input file to the database
     private static void fillDatabase(Scanner inFile, UniversityDatabase mainDatabase) {
 
-        //Go through all of the lines in the file and
+        //While the input file has another line go through all of the lines in the input file and
         while ( inFile.hasNextLine() ) {
 
-            //Set the current line of the input file to the currLine variable
-            //The variable currline will be used to process the input
+            //Set the current line of the input file to the currLine variable.
+            //The variable currline will be used to process the input.
             String currLine = inFile.nextLine();
 
-            //Depending upon the first part of the current line
+            //Depending upon the first part of the current line input, decide whether the object to be made and added
+            // to the database is a Student, Course, or a Completion.
             switch ( currLine.split("/")[0] ) {
+
+                //"STUDENT" was found at the beginning of the input line and a student object will be made.
                 case "STUDENT":
+
+
                     mainDatabase.addStudent(currLine);
                     break;
 
                 case "COMPLETION":
+
+
                     mainDatabase.addCompletionEvent(currLine);
                     break;
 
                 case "COURSE":
+
+
                     mainDatabase.addCourse(currLine);
                     break;
             }
