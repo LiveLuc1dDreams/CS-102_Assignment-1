ea83d8e9ab5808c3a00b93742aa16f96d3659779
Brandon <brantheif115@gmail.com>
2017-05-23 13:52:01 -0400

    Committing Updates:
    Added Frame for binary tree classes.
    
    -B

---
+++ src/UniversityDatabase/BinaryTree.java
@@ -0,0 +1,72 @@
+package UniversityDatabase;
+
+/**
+ * Created by brandon on 5/23/17.
+ */
+class BinaryTree implements BinaryTreeInterface {
+
+    @Override
+    public void createBinaryTree() {
+
+    }
+
+    @Override
+    public void createBinaryTree(Object rootItem) {
+
+    }
+
+    @Override
+    public void makeEmpty() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Object getRootItem() throws BinaryTreeException {
+        return null;
+    }
+
+    @Override
+    public void setRootItem(Object newItem) {
+
+    }
+
+    @Override
+    public void attachLeft(Object newItem) throws BinaryTreeException {
+
+    }
+
+    @Override
+    public void attachRight(Object newItem) throws BinaryTreeException {
+
+    }
+
+    @Override
+    public BinaryTree detachLeftSubtree() throws BinaryTreeException {
+        return null;
+    }
+
+    @Override
+    public BinaryTree detachRightSubtree() throws BinaryTreeException {
+        return null;
+    }
+
+    @Override
+    public void attachRightSubtree(BinaryTree rightTree) throws BinaryTreeException {
+
+    }
+
+    @Override
+    public void attachLeftSubtree(BinaryTree leftTree) throws BinaryTreeException {
+
+    }
+
+    @Override
+    public void createBinaryTree(Object rootItem, BinaryTree leftTree, BinaryTree rightTree) {
+
+    }
+}
---
+++ src/UniversityDatabase/BinaryTreeException.java
@@ -0,0 +1,11 @@
+package UniversityDatabase;
+
+/**
+ * Created by brandon on 5/23/17.
+ */
+class BinaryTreeException extends RuntimeException {
+
+    BinaryTreeException(String message) {
+        super(message);
+    }
+}
